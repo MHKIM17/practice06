18b217dad4ff92b0d66b16d6fd061f4c69d1a68f
MHKIM17 <mhkimfe17@gmail.com>
2016-12-01 16:47:44 +0900

    STL practice ok

---
+++ STL/STL_map.cpp
@@ -0,0 +1,20 @@
+
+#include <iostream>
+#include <string>
+#include <map>
+
+using namespace std;
+/*
+void main() {
+
+	map<string, int> m;
+
+	m["seoul"] = 100;
+	m["daegu"] = 120;
+	m["busan"] = 200;
+
+	cout << "Train to Seoul = " << m["seoul"] << "$" << endl;
+	cout << "Train to Daegu = " << m["daegu"] << "$" << endl;
+	cout << "Train to Busan = " << m["busan"] << "$" << endl;
+}
+*/
